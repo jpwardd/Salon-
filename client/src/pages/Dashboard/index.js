b1b4716507d148e07f6c88b2fbd153b813410939
jpwardd <jward9421@gmail.com>
2019-05-17 12:34:42 -0400

    file fix

---
+++ client/src/pages/Dashboard/index.js
@@ -0,0 +1,41 @@
+import React, { Fragment } from 'react'
+
+import SideNav from './SideNav'
+import Content from './Content'
+import styles from './index.css'
+
+
+
+const Dashboard = () => {
+  return (
+    
+ 
+  
+    
+    <div className="container">
+      <div>
+        <SideNav />
+      </div>
+      
+      <div>
+
+       <Content />
+      </div>
+    
+     
+    </div>
+
+
+  
+
+  
+     
+
+
+
+
+
+  )
+}
+
+export default Dashboard
